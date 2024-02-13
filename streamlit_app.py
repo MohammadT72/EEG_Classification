1b34656f5aa3e2d533330b3280a5ca81fc1cf968
Mohammad Tabarestani <87186860+MohammadT72@users.noreply.github.com>
2024-02-13 23:00:35 +0330

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,7 +75,7 @@ def select_test_files():
     files = ['Rest','Stress']
 
     # Create a selection box for the channels
-    selected_file = st.selectbox('Select test a test file', files, default='Rest')
+    selected_file = st.selectbox('Select test a test file', files)
 
     return selected_file
 
